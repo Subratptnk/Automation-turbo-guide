3b6d793d27fc60a5249d3f8e25279c508a111b4c
Subs <subratpattanaik20@gmail.com>
2022-09-19 19:27:39 +0530

    Minor Updates

--- src/test/java/subratpattanaik/testcomponents/BaseTest.java
+++ src/test/java/subratpattanaik/testcomponents/BaseTest.java
@@ -15,6 +15,8 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -34,11 +36,21 @@ public class BaseTest {
 		Properties prop = new Properties();
 		FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+"//src//main//java//subratpattanaik//resources//GlobalData.properties");
 		prop.load(fis);
-		String browserName =  prop.getProperty("browser");
+		
+		String browserName = 	System.getProperty("browser") != null ? System.getProperty("browser") : prop.getProperty("browser");
+	//	String browserName =  prop.getProperty("browser");
 		
 		if(browserName.equalsIgnoreCase("Chrome")){
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
+		}
+		else if(browserName.equalsIgnoreCase("Firefox")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new FirefoxDriver();
+		}
+		else if(browserName.equalsIgnoreCase("Edge")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new EdgeDriver();
 		}
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
